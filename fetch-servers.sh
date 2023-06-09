dfac2e9518e18ba331f64984817492b668cc38df
Max Resing <contact@maxresing.de>
2023-06-09 10:32:07 +0200

    Updated fetch-servers with some status messages

--- fetch-servers.sh
+++ fetch-servers.sh
@@ -13,6 +13,7 @@ hdfs_raw_dir="/user/hadoop/opennic/resolvers/raw/year=${yyyy}"
 hdfs_csv_dir="/user/hadoop/opennic/resolvers/csv/year=${yyyy}"
 
 extract_resolvers="extract-resolvers.py"
+hdfs="/opt/hadoop/hadoop/bin/hdfs"
 base_dir="/tmp/opennic"
 
 mkdir -p ${base_dir}
@@ -26,25 +27,29 @@ echo -n "" > "${base_tier2}.html"
 echo -n "" > "${base_tier1}.csv"
 echo -n "" > "${base_tier2}.csv"
 
+echo "Fetch server lists ..."
 # Fetch Tier 2
 curl -ks -X GET "${base_url}?tier=2" >> "${base_tier2}.html"
 
 # Fetch Tier 1
 curl -ks -X GET "${base_url}?tier=1" >> "${base_tier1}.html"
 
+echo "Extract servers to CSV ..."
 python3 $extract_resolvers "${base_tier1}.html" >> "${base_tier1}.csv"
 python3 $extract_resolvers "${base_tier2}.html" >> "${base_tier2}.csv"
 
 
 if (( $hdfs_upload == 1)) ; then
+    echo "Upload files to HDFS ..."
     # Upload raw html
-    hdfs dfs -mkdir -p "${hdfs_raw_dir}"
-    hdfs dfs -put "${base_tier1}.html" "${hdfs_raw_dir}/${yyyymmdd}.tier1.html"
-    hdfs dfs -put "${base_tier2}.html" "${hdfs_raw_dir}/${yyyymmdd}.tier2.html"
+    $hdfs dfs -mkdir -p "${hdfs_raw_dir}"
+    $hdfs dfs -put "${base_tier1}.html" "${hdfs_raw_dir}/${yyyymmdd}.tier1.html"
+    $hdfs dfs -put "${base_tier2}.html" "${hdfs_raw_dir}/${yyyymmdd}.tier2.html"
     # Upload CSVs
-    hdfs dfs -mkdir -p "${hdfs_csv_dir}"
-    hdfs dfs -put "${base_tier1}.csv" "${hdfs_csv_dir}/${yyyymmdd}.tier1.csv"
-    hdfs dfs -put "${base_tier2}.csv" "${hdfs_csv_dir}/${yyyymmdd}.tier2.csv"
+    $hdfs dfs -mkdir -p "${hdfs_csv_dir}"
+    $hdfs dfs -put "${base_tier1}.csv" "${hdfs_csv_dir}/${yyyymmdd}.tier1.csv"
+    $hdfs dfs -put "${base_tier2}.csv" "${hdfs_csv_dir}/${yyyymmdd}.tier2.csv"
+    echo "Upload files to HDFS complete"
 fi
 
 if [[ "${base_dir}" != "." ]] ; then
