436ce8897373c133df38dec3cdbaf44a85362844
Max Resing <contact@maxresing.de>
2023-06-10 10:55:25 +0200

    Updated fetch-servers to allow for a single argument

--- fetch-servers.sh
+++ fetch-servers.sh
@@ -8,6 +8,12 @@ yyyy=$(date --utc +%Y)
 yyyymmdd=$(date --utc +%Y%m%d)
 base_url="https://servers.opennic.org/"
 
+# Check if we gave a date as an argument
+if [[ "$#" == "1" ]] ; then
+  yyyy=$(date --date="$1" +%Y)
+  yyyymmdd=$(date --date="$1" +%y%m%d)
+fi
+
 # HDFS remote directory
 hdfs_raw_dir="/user/hadoop/opennic/resolvers/raw/year=${yyyy}"
 hdfs_csv_dir="/user/hadoop/opennic/resolvers/csv/year=${yyyy}"
@@ -27,12 +33,20 @@ echo -n "" > "${base_tier2}.html"
 echo -n "" > "${base_tier1}.csv"
 echo -n "" > "${base_tier2}.csv"
 
-echo "Fetch server lists ..."
-# Fetch Tier 2
-curl -ks -X GET "${base_url}?tier=2" >> "${base_tier2}.html"
+# If we have a date, fetch the raw data from HDFS, else fetch the URL
+if [[ "$#" == "1" ]] ; then
+  echo "Fetch raw HTML with tier 1 and 2 information from HDFS ..."
+  # Fetch Tier 1 and 2 from HDFS
+  hdfs dfs -get "${hdfs_raw_dir}/${yyyymmdd}.tier1.html" "${base_tier1}.html"
+  hdfs dfs -get "${hdfs_raw_dir}/${yyyymmdd}.tier2.html" "${base_tier2}.html"
+else
+  echo "Fetch server lists from web ..."
+  # Fetch Tier 2
+  curl -ks -X GET "${base_url}?tier=2" >> "${base_tier2}.html"
+  # Fetch Tier 1
+  curl -ks -X GET "${base_url}?tier=1" >> "${base_tier1}.html"
+fi
 
-# Fetch Tier 1
-curl -ks -X GET "${base_url}?tier=1" >> "${base_tier1}.html"
 
 echo "Extract servers to CSV ..."
 python3 $extract_resolvers "${base_tier1}.html" >> "${base_tier1}.csv"
